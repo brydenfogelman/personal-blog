3f1cfc5d56710b5ed31556fea69aa74c69595d06
brydenfogelman <bryden1995@gmail.com>
2016-01-12 22:34:37 -0800

    changing the style b/c i am picky

--- blog/routes/index.js
+++ blog/routes/index.js
@@ -21,10 +21,27 @@ router.get('/new_post', function(req, res, next) {
 	res.render('new_post');
 });
 
+/* GET view blog */
+router.get('/blog', function(req, res, next) {
+	var db = req.db; // this pulls the db from app.js
+	var collection = db.get('blog'); // get the db
+	// Gets all the items from the database and then sets the view
+	// the database items are passed to the view here
+	collection.find({},{},function(err, docs){
+		console.log(docs);
+		res.render('blog', {
+			"posts" : docs,
+			// "title" : 'Bryden Fogelman' 
+		});
+	});
+});
+
 /* POST to add a new post */
 router.post('/new_post', function(req, res, next) {
 	var db = req.db;
 	var title = req.body.title;
+	var user = req.body.user;
+	var abstract = req.body.abstract;
 	var content = req.body.content;
 
 	// Insert items into the database
@@ -32,6 +49,8 @@ router.post('/new_post', function(req, res, next) {
 
 	collection.insert({
 		"title" : title,
+		"user" : user,
+		"abstract" : abstract,
 		"content" : content
 	}, function(err, doc) {
 		if(err) {
@@ -39,7 +58,7 @@ router.post('/new_post', function(req, res, next) {
 			res.send('Making a new post failed.');
 		} else {
 			// on success
-			res.redirect('/');
+			res.redirect('/blog');
 		}
 	});
 });
